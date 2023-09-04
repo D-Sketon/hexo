b7751116dbb908bc374ac76e8bb81a9768c2e01c
D-Sketon <2055272094@qq.com>
2023-09-04 13:59:19 +0800

    update type

--- lib/plugins/filter/before_generate/render_post.ts
+++ lib/plugins/filter/before_generate/render_post.ts
@@ -1,6 +1,7 @@
 import Promise from 'bluebird';
+import type Hexo from '../../../hexo';
 
-function renderPostFilter() {
+function renderPostFilter(this: Hexo, data): Promise<[any[], any[]]> {
   const renderPosts = model => {
     const posts = model.toArray().filter(post => post.content == null);
 
